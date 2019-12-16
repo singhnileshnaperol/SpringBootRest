4a8440befef017dffb805dee58437dfaee729454
Nilesh Singh <singhnilesh0291@gmail.com>
2019-12-16 17:23:21 +0530

    First Commit

---
+++ src/test/java/com/example/gradlebuild/GradlebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlebuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
