c31237eb0c06613eb16e97e47cd9b7f3e3014130
Nilesh Singh <singhnilesh0291@gmail.com>
2020-01-04 01:28:28 +0530

    added rest controller class in project

--- src/main/java/com/example/gradlebuild/mainapp/GradlebuildApplication.java
+++ src/main/java/com/example/gradlebuild/mainapp/GradlebuildApplication.java
@@ -3,7 +3,7 @@ package com.example.gradlebuild.mainapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.gradlebuild.*"})
 public class GradlebuildApplication {
 
     public static void main(String[] args) {
