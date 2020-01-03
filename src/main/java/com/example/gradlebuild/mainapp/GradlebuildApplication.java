fc94f62d25d1beb239c43413de80ab2bf78afc84
Nilesh Singh <singhnilesh0291@gmail.com>
2020-01-04 01:11:46 +0530

    added elasticbeanstalk plugin in build.gradle file

--- src/main/java/com/example/gradlebuild/mainapp/GradlebuildApplication.java
+++ src/main/java/com/example/gradlebuild/mainapp/GradlebuildApplication.java
@@ -1,4 +1,4 @@
-package com.example.gradlebuild;
+package com.example.gradlebuild.mainapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
